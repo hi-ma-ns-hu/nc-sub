a7e6a1c34b7a3dc93dfb02860ea6e1852700ef2c
hi-ma-ns-hu <97180347+hi-ma-ns-hu@users.noreply.github.com>
2026-07-05 17:45:36 +0530

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseLL(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return
+        prev, curr = None, head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
+
+    def merge_node(self, head: Optional[ListNode], head3: Optional[ListNode]) -> Optional[ListNode]:
+        while head and head3:
+            nxt1 = head.next
+            nxt3 = head3.next
+            # prev.next = head
+            head.next = head3
+            head3.next = nxt1
+
+            head = nxt1
+            head3 = nxt3
+
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head:
+            return
+
+        # get midpoint
+        slow, fast = head, head
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        
+        head2 = slow.next
+        slow.next = None
+
+        # reverse the second nodelist
+        # head3 is reversed head
+        head3 = self.reverseLL(head2)
+        
+        # merge head and head3
+        self.merge_node(head, head3)
