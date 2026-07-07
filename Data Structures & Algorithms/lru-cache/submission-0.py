7eb61628c5bc85cf0d532f1986baf75bc54137cd
hi-ma-ns-hu <97180347+hi-ma-ns-hu@users.noreply.github.com>
2026-07-07 13:11:35 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, key: Optional[int] = 0, val: Optional[int] = 0, prev: 'Optional[Node]' = None, next: 'Optional[Node]' = None) -> None:
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.map = dict()
+
+        self.head = Node()
+        self.tail = Node()
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+
+    def get(self, key: int) -> int:
+        ptr = self.map.get(key, None)
+        if ptr is None:
+            return -1
+        return ptr.val
+        
+
+
+    def put(self, key: int, value: int) -> None:
+        ptr = self.map.get(key, None)
+        if ptr is None:
+            # create a new node and move its position, update the map
+            node = Node(key, value)
+            before = self.tail.prev
+            before.next = node
+            node.next = self.tail
+            self.tail.prev = node
+            node.prev = before
+            # add to map
+            self.map[key] = node
+            capacity = len(self.map)
+            if capacity > self.capacity:
+                removed_node = self.head.next
+                self.head.next = removed_node.next
+                removed_node.next.prev = self.head
+                del self.map[removed_node.key]
+            return
+        
+        # update the value
+        ptr.val = value
+        before = ptr.prev
+        after = ptr.next
+        before.next = after
+        after.prev = before
+        tail_prev = self.tail.prev
+        tail_prev.next = ptr
+        ptr.next = self.tail
+        self.tail.prev = ptr
+        ptr.prev = tail_prev
