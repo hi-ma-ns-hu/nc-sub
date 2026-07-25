773e7f2bb9adf953a573c971c6b00cbc1a420116
hi-ma-ns-hu <97180347+hi-ma-ns-hu@users.noreply.github.com>
2026-07-25 20:37:56 +0530

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        
+        def dfs(r, c, i, subset):
+            # i == len(word) means every char of word has matched
+            if i == len(word): return True
+
+            if (r < 0 or c < 0 or 
+                (r,c) in subset or # (r,c) in subset means this cell has already been visited
+                word[i] != board[r][c]): return False
+
+            subset.add((r,c))
+            # include
+            res = (dfs(r+1, c, i+1, subset) or
+            dfs(r-1, c, i+1, subset) or
+            dfs(r, c+1, i+1, subset) or
+            dfs(r, c-1, i+1, subset))
+            # backtract
+            subset.remove((r,c))
+            
+            return res
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0, set()): return True
+
+        return False
