dc5bc0a522e1e8c04848a2ba6e618ceba493ae59
hi-ma-ns-hu <97180347+hi-ma-ns-hu@users.noreply.github.com>
2026-08-08 22:44:20 +0530

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+  def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+    if endWord not in wordList: return 0
+
+    adj_list = defaultdict(list)
+    visited = set([beginWord])
+    queue = deque([beginWord])
+    res = 1
+    
+    wordList.append(beginWord)
+    for word in wordList:
+      for j in range(len(word)):
+        pattern = word[:j]+'*'+word[j+1:]
+        adj_list[pattern].append(word)
+
+    while queue:
+      for i in range(len(queue)):
+        word = queue.popleft()
+        # if word is equal to endWord just return it, you've found it
+        if word == endWord: return res
+        # now create pattern from the word to traverse other items in adj_list
+        for j in range(len(word)):
+          pattern = word[:j]+'*'+word[j+1:]
+          for k in adj_list[pattern]:
+            if k not in visited:
+              visited.add(k)
+              queue.append(k)
+        res += 1
+
+    return res
