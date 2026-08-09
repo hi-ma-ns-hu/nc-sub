51986226e7e25e77f487b2509b4f8bb4cbf3864e
hi-ma-ns-hu <97180347+hi-ma-ns-hu@users.noreply.github.com>
2026-08-09 11:45:33 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+  def climbStairs(self, n: int) -> int:
+    one, two = 1, 1 # no of ways 2nd last index and last index could climb
+    for _ in range(n-1, 0, -1):
+        # one and two both come back steps after each iteration
+        temp = one
+        one += two
+        two = temp
+    return one
