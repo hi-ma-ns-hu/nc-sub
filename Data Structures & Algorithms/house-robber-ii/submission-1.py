5dc76ea37f8ed0c4f3cfb1e7f7c0ab7db59df13b
hi-ma-ns-hu <97180347+hi-ma-ns-hu@users.noreply.github.com>
2026-08-12 22:46:30 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+  def rob(self, nums: List[int]) -> int:
+    memo = [[-1]*2 for _ in range(len(nums))]
+    def dfs(index, flag):
+        if index >= len(nums) or (flag and index == len(nums)-1): return 0
+        if memo[index][flag] != -1: return memo[index][flag]
+        memo[index][flag] = max(dfs(index+2, flag or index == 0)+nums[index],dfs(index+1, flag))
+        return memo[index][flag]
+    return max(dfs(0, True), dfs(1, False))
