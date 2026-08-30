c4fdac8e3d47fa59071d233ff4522e3ed6c5c732
hi-ma-ns-hu <97180347+hi-ma-ns-hu@users.noreply.github.com>
2026-08-30 22:24:58 +0530

    Add: reverse-integer - submission-2

---
+++ Data Structures & Algorithms/reverse-integer/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        res = int(str(abs(x))[::-1])
+
+        if x < 0: res *= -1
+        
+        if res < -1*(1 << 31) or res > (1 << 31)-1: return 0
+
+        return res
