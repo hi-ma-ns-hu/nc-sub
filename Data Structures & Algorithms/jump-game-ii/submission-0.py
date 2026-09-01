ac7ca11c8a74232864591994fcb58858b9209ca2
hi-ma-ns-hu <97180347+hi-ma-ns-hu@users.noreply.github.com>
2026-09-01 23:17:47 +0530

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        def dfs(idx):
+            if idx == len(nums)-1: return 0
+            if nums[idx] == 0: return float('inf')
+
+            end = min(len(nums)-1, idx+nums[idx])
+            
+            res = float('inf')
+            for i in range(idx+1, end+1):
+                res = min(res, 1+dfs(i))
+            return res
+
+        return dfs(0)
